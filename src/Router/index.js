35368b3286ec7316c5082895a15500d3bc55c2e5
heningyuan6256 <810151439@qq.com>
2024-03-28 10:31:05 +0800

    增加文档生成

--- src/Router/index.js
+++ src/Router/index.js
@@ -5,9 +5,35 @@ import { getList } from "../Controller/list/index.js";
 
 const router = express.Router();
 
-// 列表路由
+/**
+ * @api {post} /api/article 新建文章
+ * @apiDescription 新建文章
+ * @apiName 新建文章
+ * @apiGroup 文章
+ *
+ * @apiSampleRequest http://localhost:3000/api/article
+ *
+ * @apiParam {String} title 文章标题
+ * @apiParam {String} desc 描述
+ * @apiParamExample {json} 请求示例
+ * {
+ *   "title": "文章标题",
+ *   "desc": "描述"
+ * }
+ *
+ * @apiSuccess {Number} code 状态码
+ * @apiSuccess {Object} data 返回数据
+ * @apiSuccess {String} message 信息
+ * @apiSuccessExample {json} 响应示例
+ * {
+ *   "code": 0,
+ *   "data": {}
+ *   "message": ""
+ * }
+ *
+ * @apiVersion 1.0.0
+ */
 router.post("/list/get", getList);
-
 
 // 实例路由
 router.get("/instance/get", getInstanceInfo);
